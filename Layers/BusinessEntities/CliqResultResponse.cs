d7634adb2d8dea90db4b3115306493e889517595
qamar.nust@gmail.com <qamar.nust@gmail.com>
2017-10-23 22:14:26 +0500

    change

---
+++ Layers/BusinessEntities/CliqResultResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessEntities
+{
+    public class CliqResultResponse
+    {
+        public string Code { get; set; }
+        public string Responce { get; set; }
+    }
+}
